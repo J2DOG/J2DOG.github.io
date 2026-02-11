d0ee2a9207a6f6ce57a82be02d4648f92456ddf0
J2DOG <88285927+J2DOG@users.noreply.github.com>
2026-02-11 18:05:47 +0000

    Deploying to gh-pages from @ J2DOG/J2DOG.github.io@f9e0348c29193b8150d3b7b7e3e2c0cce821fbf9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-jiang-fengcheng-s-space",
-    title: "Jiang Fengcheng's space",
+    id: "nav-fengcheng-s-space",
+    title: "Fengcheng's space",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
